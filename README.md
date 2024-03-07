4ec90bcf632660c5eaeb6c6f42611a6878ad6e8b
Sn|cK <ido.samuelson@gmail.com>
2024-03-07 02:29:41 -0700

    cargo dev

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ this repo does not use rust standard library (no_std)
 - [ ] cargo build/run - esp32 (requires espup support for nixos)
 - [ ] nix build - fails
 - [ ] nix run - fails
+
+## current issue
+
+```sh
+nix build
+```
+= note: x86_64-unknown-linux-gnu-gcc: error: unrecognized command-line option '-flavor'
+       >           x86_64-unknown-linux-gnu-gcc: error: unrecognized command-line option '--as-needed'; did you mean '-mno-needed'?
+       >           x86_64-unknown-linux-gnu-gcc: error: unrecognized command-line option '--gc-sections'; did you mean '--data-sections'?
+       >           
+       >
+       > error: could not compile `hello_world` (bin "hello_world") due to 1 previous error
--- flake.nix
+++ flake.nix
@@ -5,12 +5,14 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs-esp-dev.url = "github:mirrexagon/nixpkgs-esp-dev";
   };
 
   outputs =
     { nixpkgs
     , flake-utils
     , rust-overlay
+    , nixpkgs-esp-dev
     , ...
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -22,6 +24,7 @@
           inherit system;
           overlays = [
             (import rust-overlay)
+            (import "${nixpkgs-esp-dev}/overlay.nix")
           ];
         };
 
@@ -52,17 +55,39 @@
         ];
 
         buildInputs = with pkgs; [
+          rustup
           rust-analyzer
+          cargo-generate
+          cargo-espflash
+          cargo-espmonitor
+
+          # slint dependencies
+          cmake
+          pkg-config
+          fontconfig
+          xorg.libxcb
+          wayland
+          libxkbcommon
+          libGL
+          # esp-idf-sys dependencies:
+          cmake
+          ninja
+          python3Packages.python
+          python3Packages.pip
+          python3Packages.virtualenv
         ];
+
       in
       {
-        devShells.default = pkgs.mkShell { inherit nativeBuildInputs buildInputs; };
-
+        devShells.default = pkgs.mkShell {
+          inherit nativeBuildInputs buildInputs;
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
+        };
 
         packages.default = rustPlatform.buildRustPackage rec {
           inherit name;
           inherit buildInputs;
-          # inherit nativeBuildInputs;
           src = ./.;
           cargoLock = {
             lockFile = ./Cargo.lock;
@@ -71,3 +96,6 @@
       }
     );
 }
+
+
+
